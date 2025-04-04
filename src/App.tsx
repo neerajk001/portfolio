1c436786ad82eaa1e6d4ee97f5c813356174933a
[Neeraj kushwaha] <[workwithneeraj.01@gmail.com]>
2025-04-04 23:52:45 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Hero from './components/Hero'
+
+
+function App() {
+  return (<>
+    <div className='min-h-screen bg-gray-900 text-white'>
+      <Hero/>
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Hero.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Hero() {
+  return (
+    <div className='relative h-screen flex justify-center items-center'>
+         <div className="absolute inset-0 bg-gradient-to-b from-purple-900/50 to-black/50 z-0" />
+            
+        
+
+    </div>
+  )
+}
+
+export default Hero
