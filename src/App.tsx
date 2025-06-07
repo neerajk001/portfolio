5b32b87b0a5fa5c4eb569dd6b9cade2d99933d35
neeraj kushwaha <workwithneeraj.01@gmail.com>
2025-06-07 16:20:20 +0530

    bottom bar added

--- src/App.tsx
+++ src/App.tsx
@@ -5,19 +5,22 @@ import Projects from './components/projects'
 import { Languages } from './components/Languages'
 import { Tools } from './components/Tools'
 import { Contact } from './components/Contact'
+import FooterBar from './components/FooterBar'
 
 
 
 function App() {
   return (<>
     <div className='min-h-screen bg-gradient-to-b from-black to-gray-800
- text-white'>
+ text-white items-center justify-center flex flex-col text-center'>
+  
       <Hero/>
       <About/>
       <Projects/>
       <Languages/>
       <Tools/>
       <Contact/>
+      <FooterBar/>
     </div>
     </>
   )
---
+++ src/components/FooterBar.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import DottedButton from './DottedButton';
+import { Home, FolderOpenDot, User } from 'lucide-react';
+
+const FooterBar: React.FC = () => {
+  return (
+    <div className="fixed bottom-6 inset-x-0 z-20 px-8 md:hidden">
+      <div className="bg-gradient-to-t from-gray-900/70 to-gray-700/30 backdrop-blur-md flex justify-around items-center py-3 rounded-xl shadow-lg max-w-3xl mx-auto opacity-90">
+        <DottedButton label ="About"icon={<Home size={20}  />} />
+        <DottedButton label='Projects' icon={<FolderOpenDot size={20} />} />
+        <DottedButton label='Hire me' icon={<User size={20} />} />
+      </div>
+    </div>
+  );
+};
+
+export default FooterBar;
