1d125568f0d0fcbfb79fffb1c85a7d39cbffb1e3
neeraj kushwaha <workwithneeraj.01@gmail.com>
2025-08-03 17:55:56 +0530

    fucked up

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,11 @@
+import React from 'react'
+import Hero from './Hero'
 
-import Hero from './components/Hero'
-import { About } from './components/About'
-import Projects from './components/projects'
-import { Languages } from './components/Languages'
-import { Tools } from './components/Tools'
-import { Contact } from './components/Contact'
-import FooterBar from './components/FooterBar'
-
-
-
-function App() {
-  return (<>
-    <div className='min-h-screen bg-gradient-to-b from-black to-gray-800
- text-white items-center justify-center flex flex-col text-center'>
-  
-      <Hero/>
-      <About/>
-      <Projects/>
-      <Languages/>
-      <Tools/>
-      <Contact/>
-      <FooterBar/>
-    </div>
-    </>
+const App = () => {
+  return (
+   <main>
+    <Hero/>
+   </main>
   )
 }
 
---
+++ src/Hero.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Hero: React.FC = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-200">
+      <h1 className="text-6xl font-bold text-black animate-bounce">
+        COMING SOON
+      </h1>
+    </div>
+  );
+};
+
+export default Hero;
